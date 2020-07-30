e879a8809784631445511f8ce663042043bcc243
Kyle Benson <kyle.benson@intercom.io>
2020-07-29 23:23:01 -0700

    optimize images, fix bugs

--- src/js.js
+++ src/js.js
@@ -6,14 +6,3 @@ function sideNav() {
   sideNav.classList.toggle('active');
   main.classList.toggle('active');
 }
-window.addEventListener("load", () => {
-  // Menu active status
-  //if (window.localStorage.getItem('sidebar') === 'active') {
-  //  var x = document.getElementById("sideNav");
-  //  var main = document.getElementById("main");
-  //  x.classList.add("active");
-  //  main.classList.add("active");
-  //}
-  // Page load animation
- document.querySelector("body").classList.add("loaded");
-});
