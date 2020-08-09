2af166979c917281e4974d36766f64d3735f95c4
Kyle Benson <kyle.benson@intercom.io>
2020-08-09 15:18:55 -0700

    Init gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio of Kyle Wayne Benson
-Built using NPM, Jekyll, and Tailwind.css via `npm start`. Hosted on github pages, deployed to `gh-pages` branch via `gulp deploy`.
--- src/js.js
+++
@@ -1,8 +0,0 @@
-
-// Side nav animation
-function sideNav() {
-  var sideNav = document.querySelector("#sideNav");
-  var main = document.querySelector("#main");
-  sideNav.classList.toggle('active');
-  main.classList.toggle('active');
-}
